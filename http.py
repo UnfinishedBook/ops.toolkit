8e8c62ffa10e0a3ef1aa699de0cc86ae0ac7a033
unfinishedbook <459291866@qq.com>
2017-09-25 20:22:32 +0800

    初步实现重启工程的http接口

--- http.py
+++ http.py
@@ -6,7 +6,7 @@
 
 from gl import *
 from wrap import *
-from bottle import route,run
+from bottle import *
 
 os.system('ssh-add /mydata/maintenance/identity/maintenance_rsa')
 output = commands.getoutput('ssh-add -l')
@@ -17,7 +17,6 @@ if 'maintenance_rsa' not in output:
 #选择要管理的环境
 env = 'test'
 GL.setEnv(env)
-
 GL.LOG = getLogger('SrvLogger', 'ops-toolkit-http.log')
 
 intro_pro = GL.conf()[GL.project()]['intro']
@@ -25,20 +24,29 @@ intro_env = GL.deploy()[GL.env()]['intro']
 intro = '进入运维工具事件循环，项目：%s，选择的环境是：%s。' % (intro_pro,intro_env)
 GL.LOG.info(intro)
 
-proj = 'quickbid-center'
-ip = '10.135.17.9'
-asked = False
+@error(404)
+def error404(error):
+    return 'Nothing here, sorry\n'
 
-mod = getMod(proj)
-restart(mod, ip, asked)
+@route('/restart', method='POST')
+def http_restart():
+    try:
+        ret = {'err':0, 'msg':''}
+        keys = request.params.keys()
+        proj = request.params.get('proj')
+        ip = request.params.get('ip')
+        mod = getMod(proj)
+        if ip not in mod.deploy():
+            raise Exception('%s not deployed in %s' % (proj,ip))
+        asked = False
+        restart(mod, ip, asked)
+        return ret
+    except Exception,e:
+        ret['err'] = 1
+        ret['msg'] = str(e)
+        return ret
 
-while(True):
-    s = raw_input('quit?:' )
-    if s == 'y':
-        break
-    else:
-        os.system('ssh-add -l')
-
+run(host='0.0.0.0', port=8887, debug=True)
 network.disconnect_all()
 
 
