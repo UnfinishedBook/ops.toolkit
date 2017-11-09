6b81d35abf1c945f1af08bb800016f6ab96d649e
unfinishedbook <459291866@qq.com>
2017-11-09 12:51:34 +0800

    增加切割pm2日志的脚本

---
+++ scripts/cut_pm2_log.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+#0 4 * * * /bin/cut_pm2_log.sh >> /var/log/cut_pm2_log.log 2>&1
+
+echo -e "Start at \c"
+date '+%F %T'
+
+LOG_DIR=/mydata/logs
+
+list=`find $LOG_DIR -name '*.log' -size +200M | grep -E '/activity-api/|/ad-api/|/ad_stats/|/api/|/auth/|/cms/|/cmsTimer/|/data/' | grep -v 'rename'`
+count=`echo $list | wc -w`
+tm=`date '+%F'`
+n=$count
+for file in $list
+do
+    cutfile=${file%.log*}-rename-${tm}.log
+    mv ${file} ${cutfile}
+    touch ${file}
+    echo "[$n/$count] 重命名 ${file} 为 ${cutfile}"
+    let n=n-1
+done
+pm2 reloadLogs
+
+echo -e "Finish at \c"
+date '+%F %T'
+
